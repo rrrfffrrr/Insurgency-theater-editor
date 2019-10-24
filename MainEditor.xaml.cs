8ca085d32d6dc88a4027aa4d3ee9d1cea7273179
강찬영 <rrrfffrrr@naver.com>
2019-10-24 17:42:28 +0900

    Add main page and editor page Add default theater files

---
+++ MainEditor.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Insurgency_theater_editor
+{
+    /// <summary>
+    /// MainEditor.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainEditor : Page
+    {
+        private static readonly string[] THEATEROPTIONS = { "<All>" };
+        private ProjectFolder PFolder;
+
+        public string CurrentFileName { get; set; }
+        private string LastContent = null;
+
+        public MainEditor()
+        {
+            InitializeComponent();
+            CurrentFileName = "File: ";
+        }
+        public MainEditor(ProjectFolder folder) : this()
+        {
+            this.PFolder = folder;
+            LoadTheater();
+            TheaterViewer.SelectedIndex = 0;
+            LoadFileTree();
+        }
+
+        /// DEV: all...
+        private FlowDocument ParseText(string text)
+        {
+            FlowDocument document = new FlowDocument();
+            Paragraph p = new Paragraph(new Run(text));
+            document.Blocks.Add(p);
+            return document;
+        }
+
+        private void LoadTheater()
+        {
+            TheaterViewer.Items.Clear();
+            foreach (string key in THEATEROPTIONS)
+            {
+                TheaterViewer.Items.Add(new ComboBoxItem() { Content = key });
+            }
+            foreach (string theater in PFolder.Theaters)
+            {
+                TheaterViewer.Items.Add(new ComboBoxItem() { Content = theater });
+            }
+        }
+        /// <summary>
+        /// Load theater file to text editor
+        /// </summary>
+        private void LoadCurrentSelectedFile()
+        {
+            Label value = FolderViewer.SelectedItem as Label;
+            if (value == null)
+                return;
+
+            if (value == null) // DEV: add validate text change
+            {
+                MessageBoxResult result = MessageBox.Show("Save current file?", "Question", MessageBoxButton.YesNoCancel, MessageBoxImage.Warning);
+                switch (result)
+                {
+                    case MessageBoxResult.Yes:
+                        // DEV: save current file
+                        break;
+                    case MessageBoxResult.No:
+                        // DEV: discard current file
+                        break;
+                    case MessageBoxResult.Cancel:
+                    default:
+                        return; // keep current file loaded
+                }
+            }
+
+            string name = value.Content.ToString().Substring(1);
+            string file = Path.Combine(PFolder.FolderPath, name);
+            string text = File.ReadAllText(file);
+
+            CurrentFileName = "File: " + name;
+            TextViewer.Document = ParseText(text);
+            LastContent = text;
+        }
+        /// <summary>
+        /// Load file tree from theater list
+        /// </summary>
+        private void LoadFileTree()
+        {
+            ComboBoxItem item = TheaterViewer.SelectedItem as ComboBoxItem;
+            string value = item.Content.ToString();
+            FolderViewer.Items.Clear();
+            string[] list;
+            if (value.CompareTo(THEATEROPTIONS[0]) == 0)
+            {
+                list = PFolder.GetFileList();
+            }
+            else
+            {
+                list = PFolder.GetFileListFromTheater(value);
+            }
+
+            foreach (string v in list)
+            {
+                FolderViewer.Items.Add(new Label() { Content = "_" + v });
+            }
+        }
+
+        private void FolderViewer_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            LoadCurrentSelectedFile();
+        }
+
+        private void TheaterViewer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LoadFileTree();
+        }
+    }
+}
---
+++ StartPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Collections;
+using System.Globalization;
+using System.Resources;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace Insurgency_theater_editor
+{
+    /// <summary>
+    /// StartPage.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OpenProjectFolder(object sender, System.Windows.RoutedEventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    var pf = new ProjectFolder(fbd.SelectedPath);
+                    var page = new MainEditor(pf);
+
+                    this.NavigationService.Navigate(page);
+
+                    var window = System.Windows.Window.GetWindow(this);
+                    window.Title = "Insurgency theater editor : " + pf.FolderPath;
+                }
+            }
+        }
+
+        private void Button_CreateNewProject(object sender, System.Windows.RoutedEventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    /// DEV: Make dialog to get new theater name
+                    string theater = Microsoft.VisualBasic.Interaction.InputBox("New theater name", "Theater name input box", "default");
+                    if (string.IsNullOrWhiteSpace(theater) || theater.Contains(' ') || theater.Contains('\t'))
+                    {
+                        MessageBox.Show("Cannot use theater name with whitespace character", "Input error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    ResourceManager manager = new ResourceManager(typeof(Properties.Resources));
+                    ResourceSet set = manager.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
+                    foreach(DictionaryEntry entry in set)
+                    {
+                        string path;
+                        if (entry.Key.ToString().CompareTo("theater") == 0)
+                        {
+                            path = theater + ".theater";
+                        }
+                        else
+                        {
+                            path = theater + "_" + entry.Key.ToString() + ".theater";
+                        }
+                        path = Path.Combine(fbd.SelectedPath, path);
+                        string content = Encoding.UTF8.GetString(entry.Value as byte[]).Replace("{theater}", theater);
+                        File.WriteAllText(path, content);
+                    }
+
+                    var pf = new ProjectFolder(fbd.SelectedPath);
+                    var page = new MainEditor(pf);
+
+                    this.NavigationService.Navigate(page);
+
+                    var window = System.Windows.Window.GetWindow(this);
+                    window.Title = "Insurgency theater editor : " + pf.FolderPath;
+                }
+            }
+        }
+    }
+}
